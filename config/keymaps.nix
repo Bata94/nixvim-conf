7013920f8ea8ea9efa5c1ca04f499f363cb4a200
Bastian Sievers <b.sievers@sits.ruhr>
2024-05-17 01:57:18 +0200

    added keymaps

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -51,6 +51,7 @@
       };
     }
 
+    # Navigate Vim Panes
     {
       key = "<C-h>";
       action = ":wincmd h<CR>";
@@ -88,8 +89,18 @@
       };
     }
 
+    # Move Highlighted Lines
     {
       key = "K";
+      action = ":m '<-2<CR>gv=gv";
+      mode = "v";
+      options = {
+        silent = true;
+        desc = "Move line up";
+      };
+    }
+    {
+      key = "<s-UP>";
       action = ":m '<-2<CR>gv=gv";
       mode = "v";
       options = {
@@ -104,6 +115,144 @@
       options = {
         silent = true;
         desc = "Move line down";
+      };
+    }
+    {
+      key = "<s-DOWN>";
+      action = ":m '>+1<CR>gv=gv";
+      mode = "v";
+      options = {
+        silent = true;
+        desc = "Move line down";
+      };
+    }
+
+    # While searching Cursor stays in the middle
+    {
+      key = "n";
+      action = "nzzzV";
+      mode = "n";
+      options = {
+        silent = true;
+      };
+    }
+    {
+      key = "N";
+      action = "NzzzV";
+      mode = "n";
+      options = {
+        silent = true;
+      };
+    }
+
+    # Moving half pages with Cursor in the middle
+    {
+      key = "<C-d>";
+      action = "<C-d> zz";
+      mode = "n";
+      options = {
+        silent = true;
+      };
+    }
+    {
+      key = "<C-u>";
+      action = "<C-u> zz";
+      mode = "n";
+      options = {
+        silent = true;
+      };
+    }
+
+    # Paste, but keep buffer
+    {
+      key = "<leader>p";
+      action = "\"_dP";
+      mode = "x";
+      options = {
+        silent = true;
+        desc = "Paste, but keep buffer";
+      };
+    }
+
+    # Yank to Systemclipboard
+    {
+      key = "<leader>y";
+      action = "\"+Y";
+      mode = "n";
+      options = {
+        silent = true;
+        desc = "Yank Line to Systemclipboard";
+      };
+    }
+    {
+      key = "<leader>y";
+      action = "\"+y";
+      mode = "v";
+      options = {
+        silent = true;
+        desc = "Yank selected to Systemclipboard";
+      };
+    }
+
+    # Delete to void
+    {
+      key = "<leader>d";
+      action = "\"_d";
+      mode = "n";
+      options = {
+        silent = true;
+        desc = "Delete to void";
+      };
+    }
+    {
+      key = "<leader>d";
+      action = "\"_d";
+      mode = "v";
+      options = {
+        silent = true;
+        desc = "Delete to void";
+      };
+    }
+
+    # Easy Search and Replace
+    {
+      key = "<leader>s";
+      action = "[[:%s/\/<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]]";
+      mode = "n";
+      options = {
+        silent = true;
+        desc = "Easy Search and Replace";
+      };
+    }
+
+    # Delete single char to void
+    {
+      key = "x";
+      action = "\"_x";
+      mode = "n";
+      options = {
+        silent = true;
+        desc = "Delete char to void";
+      };
+    }
+
+    # Incre-/Decrement numbers
+    {
+      key = "<leader>+";
+      action = "<C-a>";
+      mode = "n";
+      options = {
+        silent = true;
+        desc = "Increment number";
+      };
+    }
+    {
+      key = "<leader>-";
+      action = "<C-x>";
+      mode = "n";
+      options = {
+        silent = true;
+        desc = "Decrement number";
       };
     }
   ];
