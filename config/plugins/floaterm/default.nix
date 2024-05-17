a96f9c862f046d139112f859c223f80588f35f7e
Bastian Sievers <b.sievers@sits.ruhr>
2024-05-17 14:13:28 +0200

    added keymaps to floaterm

--- config/plugins/floaterm/default.nix
+++ config/plugins/floaterm/default.nix
@@ -2,13 +2,17 @@
   plugins.floaterm = {
     enable = true;
     autoclose = 0;
-    title = "";
+    # title = "";
 
     width = 0.9;
     height = 0.9;
 
     keymaps = {
       toggle = "<C-b>";
+      new = "<C-t>";
+      kill = "<C-w>";
+      next = "<C-l>";
+      prev = "<C-h>";
     };
   };
 }
