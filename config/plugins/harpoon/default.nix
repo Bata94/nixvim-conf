2aca35d3ea1cb35006cdd3f1e308ca6446b83ab8
Bastian Sievers <b.sievers@sits.ruhr>
2024-06-08 18:54:42 +0200

    testing harpoon

--- config/plugins/harpoon/default.nix
+++ config/plugins/harpoon/default.nix
@@ -1,22 +1,30 @@
+{pkgs, ...}:
 {
   plugins.harpoon = {
     enable = true;
-    enableTelescope = true;
 
+    package = pkgs.vimPlugins.harpoon;
+
+    enableTelescope = false;
+    enterOnSendcmd = true;
+
+    projects = {};
+
+    keymapsSilent = true;
     keymaps = {
-      addFile = "fa";
-      cmdToggleQuickMenu = "fe";
+      addFile = "<leader>fa";
+      cmdToggleQuickMenu = "<leader>fe";
       navFile = {
         "1" = "<C-1>";
         "2" = "<C-2>";
         "3" = "<C-3>";
         "4" = "<C-4>";
-        "5" = "<C-5>";
-        "6" = "<C-6>";
-        "7" = "<C-7>";
-        "8" = "<C-8>";
-        "9" = "<C-9>";
-        "0" = "<C-0>";
+        # "5" = "<C-5>";
+        # "6" = "<C-6>";
+        # "7" = "<C-7>";
+        # "8" = "<C-8>";
+        # "9" = "<C-9>";
+        # "10" = "<C-0>";
       };
     };
   };
