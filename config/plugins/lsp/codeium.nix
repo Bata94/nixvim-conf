00f84ddcec862e23bf37843148b93dd580878a15
Bastian Sievers <b.sievers@sits.ruhr>
2024-11-20 12:19:55 +0100

    codeium added chat capa

--- config/plugins/lsp/codeium.nix
+++ config/plugins/lsp/codeium.nix
@@ -1,5 +1,15 @@
 {
   plugins.codeium-nvim = {
     enable = true;
+    settings = {
+      enable_chat = true;
+    };
   };
+
+  keymaps = [
+    {
+      key = "<leader>cc";
+      action = "<cmd>Codeium Chat<CR>";
+    }
+  ];
 }
