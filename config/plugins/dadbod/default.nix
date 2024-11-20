7b134f12952e94b47ca6fe9b62203000ac7202cb
Bastian Sievers <b.sievers@sits.ruhr>
2024-11-20 11:32:10 +0100

    added dadbod keybind

--- config/plugins/dadbod/default.nix
+++ config/plugins/dadbod/default.nix
@@ -4,6 +4,17 @@
     vim-dadbod
     vim-dadbod-completion
     vim-dadbod-ui
-  ]
-;
+  ];
+  
+  keymaps = [
+    {
+      key = "<leader>db";
+      action = "<cmd>tabnew<CR><cmd>DBUI<CR>";
+      mode = "n";
+      options = {
+        silent = true;
+        desc = "Open Oil";
+      };
+    }
+  ];
 }
