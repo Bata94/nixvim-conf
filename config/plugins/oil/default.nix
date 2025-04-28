a287357a4d1113ca855b39f45f4d3d4677434782
Bastian Sievers <b.sievers@sits.ruhr>
2025-04-28 12:33:34 +0200

    uncommited running changes

--- config/plugins/oil/default.nix
+++ config/plugins/oil/default.nix
@@ -8,7 +8,7 @@
         # "size"
         # "permissions"
       ];
-      default_file_explorer = true;
+      default_file_explorer = false;
       skip_confirm_for_simple_edits = true;
       view_options = {
         show_hidden = true;
